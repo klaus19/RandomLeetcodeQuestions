e884b7b3270e4844ea63bfb07fabb3dc6a8cc28c
tejaskhartude <tejaskhartude@gmail.com>
2022-08-15 11:09:40 +0530

    Added Meeting Room 2 question

--- src/Heapquestions/MeetingRoom2.java
+++ src/Heapquestions/MeetingRoom2.java
@@ -16,35 +16,15 @@ public class MeetingRoom2 {
            if (intervals==null ||intervals.length==0){
                return 0;
            }
-
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[0]-b[0];
-            }
-        });
-
-        PriorityQueue<int[]>pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[1]=b[1];
-            }
-        });
-
-        pq.add(intervals[0]);
-        for (int i=1;i<intervals.length;i++){
-            int []curr = intervals[i];
-
-            int[]prev = pq.poll();
-            if (curr[0]<prev[1]){
-                pq.add(prev);
-                pq.add(curr);
-
-            }else{
-                prev[1]=curr[1];
-                pq.add(prev);
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.offer(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            minHeap.offer(intervals[i][1]);
+            if (minHeap.peek() <= intervals[i][0]) {
+                minHeap.poll();
             }
         }
-         return pq.size();
+        return minHeap.size();
     }
 }
