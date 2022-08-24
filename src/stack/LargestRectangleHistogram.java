200685c13e91701f4d198ccbd5341f952e8ca71a
tejaskhartude <tejaskhartude@gmail.com>
2022-08-24 15:22:30 +0530

    Lrgest Rectangle of hISTOGRAM

---
+++ src/stack/LargestRectangleHistogram.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+//Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
+// return the area of the largest rectangle in the histogram.
+public class LargestRectangleHistogram {
+
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1)
+                    && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+
+
+    }
+
+
+}
